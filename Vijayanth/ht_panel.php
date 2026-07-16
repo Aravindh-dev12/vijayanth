9b4555adb73a33325be1ccf1267beb32c11c7472
Aravindhan <104446384+Aravindh-dev12@users.noreply.github.com>
2026-07-16 10:46:35 +0530

    Make all dashboards responsive

--- Vijayanth/ht_panel.php
+++ Vijayanth/ht_panel.php
@@ -27,6 +27,7 @@ $htFirstInvX = $htMainCenterX - (($htInverterCount - 1) * $htInvSpacing / 2);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/responsive.css">
     <title id="pageTitle">Solar Plant - SLD</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
