a15a6e1f7f8fccdc93ecba23e2de504e0d45bf8e
Aravindhan <104446384+Aravindh-dev12@users.noreply.github.com>
2026-07-22 08:58:04 +0530

    Refresh Overview force placement cache version

--- Vijayanth/check_auth.php
+++ Vijayanth/check_auth.php
@@ -62,7 +62,7 @@ ob_start(function ($html) use ($signedPlantIdJson, $signedRoleJson, $signedConfi
         $injection .= "\n<script src=\"assets/inverter3_fix.js?v=20260721-4\"></script>";
         $injection .= "\n<script src=\"assets/overview_ui_runtime_fix.js?v=20260721-7\"></script>";
         $injection .= "\n<script src=\"assets/overview_week_generation.js?v=20260721-2\"></script>";
-        $injection .= "\n<script src=\"assets/overview_force_plant_info_position.js?v=20260721-1\"></script>";
+        $injection .= "\n<script src=\"assets/overview_force_plant_info_position.js?v=20260721-2\"></script>";
     }
 
     if ($currentPage === 'sld.php') {
